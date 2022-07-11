45348beb03869c46f7be64b1b167edece7a6ad81
manasdangi <manasraj9669@gmail.com>
2022-07-11 11:12:34 +0530

    some changes

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,27 @@ const weatherform = document.querySelector('form');
 const input = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
+const messageThree = document.querySelector('#message-3');
+const messageFour = document.querySelector('#message-4');
+
 weatherform.addEventListener('submit', (e) => {
   e.preventDefault();
   console.log(input.value);
-  //   console.log('testing');
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
   var city = input.value;
   input.value = '';
-  fetch('/weather?address=' + city).then((response) => {
+  fetch('http://localhost:3000/weather?address=' + city).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
       } else {
         messageOne.textContent = data.update.country;
-        messageTwo.textContent = data.update.temperature;
+        messageTwo.textContent =
+          'There is ' + data.update.temperature + ' degree celsuis';
+        messageThree.textContent = 'There is ' + data.update.weather_condition;
+        messageFour.textContent =
+          'There is ' + data.update.temperature + ' percentage rain possible';
       }
     });
   });
